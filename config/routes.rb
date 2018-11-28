f2e4ef6ce282c704795ba85b50e0ce13f7d81436
Tymm Schmitke <ifellinaholeonce@gmail.com>
2018-11-28 11:38:25 -0500

    Limit Search Endpoint Results

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :publishing_houses
   resources :authors
   resources :books
+  get '/search', to: 'search#show', as: :search
   post '/github', to: 'github_webhooks#webhook', as: :github_webhook
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/search_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SearchControllerTest < ActionDispatch::IntegrationTest
+  class GetShow < SearchControllerTest
+    test ' it returns JSON results' do
+      get search_path(q: 'MyStr')
+      json = JSON.parse(response.body)
+      assert_equal 2, json['books'].count
+      assert_equal 1, json['authors'].count
+    end
+  end
+end
